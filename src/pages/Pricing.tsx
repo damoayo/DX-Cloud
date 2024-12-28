fcd925b9225ddbba63a888d34feef75d284de59c
MJ JI <mamapai@naver.com>
2024-12-29 00:05:57 +0900

    main,pricing 페이지수정

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -3,12 +3,14 @@ import PriceCom from "../components/Price/PriceCom";
 
 const Pricing = () => {
   return (
-    <>
+    <div>
       <Navbar />
-      <div >
+      <div className="flex h-[100vh] justify-center mt-10">
         <PriceCom />
+        <br />
+        <br />
       </div>
-    </>
+    </div>
   );
 };
 
